181241ce5c5ff9bc39d92ef1ed4d7520e051ecfb
TypxaH <turhan@abv.bg>
2022-06-11 11:48:13 +0300

    Replace raw with row

--- Problem6/src/Main.java
+++ Problem6/src/Main.java
@@ -23,7 +23,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number of matrix raws: ");
+        System.out.print("Enter the number of matrix rows: ");
         int n = scanner.nextInt();
         System.out.print("Enter the number of matrix columns: ");
         int m = scanner.nextInt();
